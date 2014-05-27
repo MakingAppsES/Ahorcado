44ef69f9d1ed21c4b7fb6d8c08554f5f0d51e77d
Carranza <oz.carranza@gmail.com>
2014-05-27 11:22:29 +0200

    Intent hacia GameActivity.

--- app/src/main/java/com/example/ahorcado/MainActivity.java
+++ app/src/main/java/com/example/ahorcado/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.ahorcado;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -17,42 +18,45 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Button list
+        // Lista de botones
         ArrayList<Button> botones = new ArrayList<Button>();
-        // Fill button list
+        // Rellenar la lista de botones
         botones.add((Button) findViewById(R.id.btn_about));
         botones.add((Button) findViewById(R.id.btn_new_game));
         botones.add((Button) findViewById(R.id.btn_settings));
         botones.add((Button) findViewById(R.id.btn_statistics));
 
-        // Font path
+        // Ruta de la fuente
         String fontPath = "fonts/FFF_Tusj.ttf";
-        // Loading Font Face
+        // Carga de la fuente
         Typeface tf = Typeface.createFromAsset(getAssets(), fontPath);
 
-        // Applying font to buttons
+        // Aplicando la fuente a los botones
         for(Button b : botones)
             b.setTypeface(tf);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
         if (id == R.id.action_settings) {
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
+    public void newGame(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+
+        startActivity(intent);
+    }
 }
