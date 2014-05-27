484ee7331228ac75d06e05e646ec768278de8a54
antorof <antorof.dev@gmail.com>
2014-05-27 12:31:55 +0200

    Añadido teclado a GameActivity

--- app/src/main/java/com/example/ahorcado/GameActivity.java
+++ app/src/main/java/com/example/ahorcado/GameActivity.java
@@ -1,9 +1,14 @@
 package com.example.ahorcado;
 
+import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class GameActivity extends ActionBarActivity {
 
@@ -11,6 +16,47 @@ public class GameActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        // Lista de botones
+        ArrayList<TextView> botonesLetras = new ArrayList<TextView>();
+        // Rellenar la lista de botonesLetras
+        botonesLetras.add((TextView) findViewById(R.id.txt_a));
+        botonesLetras.add((TextView) findViewById(R.id.txt_b));
+        botonesLetras.add((TextView) findViewById(R.id.txt_c));
+        botonesLetras.add((TextView) findViewById(R.id.txt_d));
+        botonesLetras.add((TextView) findViewById(R.id.txt_e));
+        botonesLetras.add((TextView) findViewById(R.id.txt_f));
+        botonesLetras.add((TextView) findViewById(R.id.txt_g));
+        botonesLetras.add((TextView) findViewById(R.id.txt_h));
+        botonesLetras.add((TextView) findViewById(R.id.txt_i));
+        botonesLetras.add((TextView) findViewById(R.id.txt_j));
+        botonesLetras.add((TextView) findViewById(R.id.txt_k));
+        botonesLetras.add((TextView) findViewById(R.id.txt_l));
+        botonesLetras.add((TextView) findViewById(R.id.txt_m));
+        botonesLetras.add((TextView) findViewById(R.id.txt_n));
+        botonesLetras.add((TextView) findViewById(R.id.txt_o));
+        botonesLetras.add((TextView) findViewById(R.id.txt_p));
+        botonesLetras.add((TextView) findViewById(R.id.txt_q));
+        botonesLetras.add((TextView) findViewById(R.id.txt_r));
+        botonesLetras.add((TextView) findViewById(R.id.txt_s));
+        botonesLetras.add((TextView) findViewById(R.id.txt_t));
+        botonesLetras.add((TextView) findViewById(R.id.txt_u));
+        botonesLetras.add((TextView) findViewById(R.id.txt_v));
+        botonesLetras.add((TextView) findViewById(R.id.txt_w));
+        botonesLetras.add((TextView) findViewById(R.id.txt_x));
+        botonesLetras.add((TextView) findViewById(R.id.txt_y));
+        botonesLetras.add((TextView) findViewById(R.id.txt_z));
+
+        // Ruta de la fuente
+        String fontPath = "fonts/FFF_Tusj.ttf";
+        // Carga de la fuente
+        Typeface tf = Typeface.createFromAsset(getAssets(), fontPath);
+
+        // Aplicando la fuente y el tamanio a los botones
+        for(TextView b : botonesLetras) {
+            b.setTypeface(tf);
+            b.setTextSize(TypedValue.COMPLEX_UNIT_SP, 26);
+        }
     }
 
     @Override
