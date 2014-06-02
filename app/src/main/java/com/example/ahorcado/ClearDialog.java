6262bf93b5cfddf8828c5d70a48f3cb777d1b12b
antorof <antorof.dev@gmail.com>
2014-06-02 12:23:42 +0200

    ClearDialog background transparente

--- app/src/main/java/com/example/ahorcado/ClearDialog.java
+++ app/src/main/java/com/example/ahorcado/ClearDialog.java
@@ -2,6 +2,7 @@ package com.example.ahorcado;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -16,6 +17,7 @@ public class ClearDialog extends Dialog {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
     }
 
     public ClearDialog(Context context) {
