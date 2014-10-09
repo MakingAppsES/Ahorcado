e0b54251e8895756393e0b48067bec117becf90c
antorof <antorof.dev@gmail.com>
2014-10-09 10:31:34 +0200

    Nuevos strings, nueva versión y limpieza de AcercaDe.

--- app/src/main/java/es/makingapps/ahorcado/AcercaDeActivity.java
+++ app/src/main/java/es/makingapps/ahorcado/AcercaDeActivity.java
@@ -25,38 +25,7 @@ public class AcercaDeActivity extends ActionBarActivity {
         ((TextView)findViewById(R.id.tv_esen)).setTypeface(tf);
         ((TextView)findViewById(R.id.tv_description)).setTypeface(tf);
         ((TextView)findViewById(R.id.tv_developers)).setTypeface(tf);
-        /*((TextView)findViewById(es.makingapps.ahorcado.R.id.tv_github)).setTypeface(tf);
-        ((TextView)findViewById(es.makingapps.ahorcado.R.id.tv_github_antorof)).setTypeface(tf);
-        ((TextView)findViewById(es.makingapps.ahorcado.R.id.tv_github_carranza)).setTypeface(tf);
-        ((TextView)findViewById(es.makingapps.ahorcado.R.id.tv_github_url)).setTypeface(tf);
-        ((TextView)findViewById(es.makingapps.ahorcado.R.id.tv_github_antorof_url)).setTypeface(tf);
-        ((TextView)findViewById(es.makingapps.ahorcado.R.id.tv_github_carranza_url)).setTypeface(tf);*/
         ((TextView)findViewById(R.id.tv_version)).setTypeface(tf);
-
-//        View.OnClickListener linkClickListener = new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                String dir_url = "";
-//                switch (view.getId()){
-//                    case es.makingapps.ahorcado.R.id.tv_github_url:
-//                            dir_url = "https://github.com/MakingAppsES/Ahorcado";
-//                        break;
-//                    case es.makingapps.ahorcado.R.id.tv_github_antorof_url:
-//                        dir_url = "https://github.com/antorof";
-//                        break;
-//                    case es.makingapps.ahorcado.R.id.tv_github_carranza_url:
-//                        dir_url = "https://github.com/Carranza";
-//                        break;
-//                }
-//                Uri uri = Uri.parse(dir_url);
-//                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-//                AcercaDeActivity.this.startActivity(intent);
-//            }
-//        };
-
-//        findViewById(es.makingapps.ahorcado.R.id.tv_github_url).setOnClickListener(linkClickListener);
-//        findViewById(es.makingapps.ahorcado.R.id.tv_github_antorof_url).setOnClickListener(linkClickListener);
-//        findViewById(es.makingapps.ahorcado.R.id.tv_github_carranza_url).setOnClickListener(linkClickListener);
     }
 
 
